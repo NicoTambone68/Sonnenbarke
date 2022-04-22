60439499be656004653bc237a1c1a5d6f658b456
Nicolò Tambone <nick@adcc-lab.subnet.vcn.oraclevcn.com>
2022-04-22 10:44:35 +0000

    improved test.sh, added a test

--- test.sh
+++ test.sh
@@ -1,5 +1,41 @@
-printf "Starting automatic tests\n"
-printf "Be sure to have ra1, ra2, ra3 started in other shells, but not ra4\n"
+printf "Starting automatized tests\n"
+printf "Be sure to have separate shells for ra1, ra2, ra3 and NOT ra4 \n"
+
+printf "Checking cluster nodes...\n"
+
+RA1=`ps -ef | grep rebar3 | awk '/ra1/' | wc -l`
+RA2=`ps -ef | grep rebar3 | awk '/ra2/' | wc -l`
+RA3=`ps -ef | grep rebar3 | awk '/ra3/' | wc -l`
+RA4=`ps -ef | grep rebar3 | awk '/ra4/' | wc -l`
+
+CHK=0
+
+if [ $RA1 -lt 1 ]; then
+	printf "node ra1 must be ready!\n"
+	CHK=-1
+fi
+
+if [ $RA2 -lt 1 ]; then
+	printf "node ra2 must be ready!\n"
+	CHK=-1
+fi
+
+if [ $RA3 -lt 1 ]; then
+	printf "node ra3 must be ready!\n"
+	CHK=-1
+fi
+
+if [ $RA4 -ge 1 ]; then
+	printf "node ra4 must NOT be ready!\n"
+	CHK=-1
+fi
+
+if [ $CHK -lt 0 ]; then
+  printf "ERROR: One or more nodes not available. Please check!\n"	
+  exit -1
+fi
+
+printf "Nodes ok, now starting tests...\n"
 
 export FQDN=`hostname --fqdn`
 
@@ -9,11 +45,11 @@ rm -rf _build/test
 
 sleep 2s
 
-export hostname="ra4@${FQDN}"
+export nodename="ra4@${FQDN}"
 
-printf "Running test as %s\n" $hostname
+printf "Running test as %s\n" $nodename
 
-rebar3 ct --name=$hostname --suite=test/sb_SUITE
+rebar3 ct --name=$nodename --suite=test/sb_SUITE
 
 
 printf "Done. Cheers\n"
--- test/sb_SUITE.erl
+++ test/sb_SUITE.erl
@@ -17,7 +17,7 @@
         sb_interface_1_test/1,
         sb_interface_2_test/1,
         sb_interface_3_test/1,
-	sb_cluster_test/1,
+        sb_node_replication_test/1,
 	datafile_exists/2
 	]).
 
@@ -27,12 +27,12 @@ all() -> [{group, public}].
 
 groups() -> [
 	     {public, [shuffle], [
-				  sb_basic_test,
-				  sb_metadata_test,
-				  sb_interface_1_test,
-				  sb_interface_2_test,
-				  sb_interface_3_test
-%				  sb_cluster_test
+%				  sb_basic_test,
+%				  sb_metadata_test,
+%				  sb_interface_1_test,
+%				  sb_interface_2_test,
+%				  sb_interface_3_testi,
+                                  sb_node_replication_test
 				 ]}
 	    ].
 
@@ -197,92 +197,83 @@ sb_interface_3_test(_Config) ->
 
    ok.
 
-% TO DO: remove
-sb_cluster_test(_Config) ->
-   
-   % get cluster nodes from env 
-   {ok,L} = application:get_env(system_metadata, cluster),
-   [{nodes, Nodes}] = lists:filter(fun({X,_}) -> X == nodes end, L),
 
-   % get the Fully Qualified Domain Name
-   %{ok, FQDN} = net_adm:dns_hostname(net_adm:localhost()),
 
-   % Node names
-   % Node1  = list_to_atom(atom_to_list('ra1@') ++ FQDN),
-   %Node2  = list_to_atom(atom_to_list('ra2@') ++ FQDN),
-  
-   Node1 = lists:nth(1, Nodes), 
-
-   % create new  metadata
+sb_node_replication_test(_Config) ->
+   % Create new  metadata
    sbcli:create_cluster_metadata(),
-
-   % start
-   sbcli:start(),
-   
-   timer:sleep(2000),
-
-   sbcli:new(sb_cluster_test),
-
-   sbcli:addNode(sb_cluster_test, Node1),
-
-   sbcli:out(sb_cluster_test, {aaaa}),
-
-   % cut off node1 
-   %sbcli:stop_node(Node1),
-
-   % set an invalid cookie to avoid reconnection
-%   erlang:set_cookie(Node1, invalid_cookie),
-   
-   % disconnect Node1 and check disconnection
-   ?assertMatch(true, erlang:disconnect_node(Node1)),
-   
-   %sbcli:addNode(sb_cluster_test, Node2),
-   
-   timer:sleep(500),
-   
-   %?assertMatch(pang, net_adm:ping(Node1)),
-   
-   sbcli:out(sb_cluster_test, {bbbb}),
-
-   timer:sleep(500),
-
-   sbcli:out(sb_cluster_test, {cccc}),
-
-   % rejoin node
-   %net_kernel:start([Node1]),
-   
-   %timer:sleep(1000),
-
-   % reconnect   
-%   erlang:set_cookie(Node1, erlang:get_cookie()),
-   ?assertMatch(pong, net_adm:ping(Node1)),
-
-   erpc:call(Node1, sb, restart, []),
    
    timer:sleep(1000),
 
-   %%% Now check: all nodes' metadata must be equal
+   % Start the cluster
+   sbcli:start(),
 
-   % get cluster nodes from env 
-   %{ok,L} = application:get_env(system_metadata, cluster),
-   %[{nodes, Nodes}] = lists:filter(fun({X,_}) -> X == nodes end, L),
+   timer:sleep(1000),
+
+   {ok, Ln} = application:get_env(system_metadata, cluster),
+   [{nodes, Nodes}] = lists:filter(fun({X,_}) -> X == nodes end, Ln),
+
+   TS = adcc_node_repl_test,
+
+   NumberOfTuples = 100,
+
+   % Create a new tuple space
+   sbcli:new(TS),
+
+   Node = get_random_node(Nodes, node()),
+
+   % List of the replicated Nodes
+   ReplNodes = [Node, node()],
    
-   % get a list of each node metadata
-   %NList = [erpc:call(N, sbcli, metadata, []) || N <- Nodes],
+   % List of the not replicated Nodes
+   NotReplNodes = [N || N <- Nodes, (lists:member(N, ReplNodes) =:= false)],
 
-   % a node's metadata must be equal to each other node's metadata
-   %?assert(lists:all(fun(X) -> X =:= lists:nth(1, NList) end, NList), "Metadata must be equal to each other's node"),
+   % Replicate the tuple space on another random node 
+   sbcli:addNode(TS, Node),
+
+   timer:sleep(1000),
+
+   % The TS's datafile now must exists on all the replicated nodes
+   [?assertMatch(true, ?MODULE:datafile_exists(TS, N)) || N <- ReplNodes],
+
+   % The TS's datafile now must NOT exists on all the other nodes
+   [?assertMatch(false, ?MODULE:datafile_exists(TS, N)) || N <- NotReplNodes],
+
+   % Generate a list of random tuples   
+   ListOfTuples = [{Idx, rand:uniform(1000)} || Idx <- lists:seq(1, NumberOfTuples)],
+
+   % Insert the tuples on the TS
+   [sbcli:out(TS, T) || T <- ListOfTuples],
    
-   erpc:call(Node1, sbcli, rd, [sb_cluster_test, {aaaa}, 200]),
+   % Allow some time to flush the buffers
+   timer:sleep(1000),
 
-   ?assertMatch([{aaaa}], erpc:call(Node1, sbcli, rd, [sb_cluster_test, {aaaa}, 200])),
-   ?assertMatch([{bbbb}], erpc:call(Node1, sbcli, rd, [sb_cluster_test, {bbbb}, 200])),
-   ?assertMatch([{cccc}], erpc:call(Node1, sbcli, rd, [sb_cluster_test, {cccc}, 200])),
+   % Check data integrity. Stored data on the TS must be the same on all nodes replica of the TS
+   [[?assertMatch(true, lists:member(lists:nth(Elem, ListOfTuples), sbcli:select_all(TS, N))) 
+                  || Elem <- lists:seq(1, NumberOfTuples)] || N <- ReplNodes],
+  
 
-   % stop
+   % Now remove the TS from all the replicated nodes
+   [sbcli:removeNode(TS, N) || N <- ReplNodes],
+   
+   timer:sleep(1000),
+
+   % The TS datafile must be gone on every node
+   [?assertMatch(false, ?MODULE:datafile_exists(TS, N)) || N <- Nodes],
+
+   % Done. Stop the cluster 
    sbcli:stop(),
+
    ok.
 
+
+% selects from the list Nodes a random node different than Node
+get_random_node(Nodes, Node) ->
+   Index = rand:uniform(length(Nodes)),
+   case N = lists:nth(Index, Nodes) of
+      Node -> get_random_node(Nodes, Node);
+	_  -> N
+   end.
 
 
 
